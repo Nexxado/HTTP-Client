14408c78ef9a6559bc8f9e4ca9deffc966f705db
ND88 Vivobook Ubuntu <ndraiman@gmail.com>
2015-11-28 16:52:34 +0200

    progress on input verification

--- client.c
+++ client.c
@@ -17,6 +17,7 @@ void initClient();
 char* getCMD();
 char** parseCMD(char*);
 int checkFlags(char*);
+int checkTimeIntervalFormat(char*, char*);
 void destroy_tokens(char**);
 
 //DEBUG Methods
@@ -27,9 +28,9 @@ void printTokens(char**);
 
 int main() {
 
-  initClient();
+        initClient();
 
-  return 0;
+        return 0;
 }
 
 /******************************************************************************/
@@ -38,24 +39,24 @@ int main() {
 
 void initClient() {
 
-  char* input;
-  char** cmdTokens = NULL;
+        char* input;
+        char** cmdTokens = NULL;
 
-  do {
+        do {
 
-    destroy_tokens(cmdTokens);
-    printf("cmd: ");
-    input = getCMD();
-    printf("input = %s\n", input); //DEBUG
+                destroy_tokens(cmdTokens);
+                printf("cmd: ");
+                input = getCMD();
+                // printf("input = %s\n", input); //DEBUG
 
-  } while((cmdTokens = parseCMD(input)) == NULL);
+        } while((cmdTokens = parseCMD(input)) == NULL);
 
-  printTokens(cmdTokens); //DEBUG
-
-
+        // printTokens(cmdTokens); //DEBUG
 
 
-  destroy_tokens(cmdTokens);
+
+        free(input);
+        destroy_tokens(cmdTokens);
 }
 
 /******************************************************************************/
@@ -66,36 +67,36 @@ void initClient() {
 //Method to handle user input of unknown length while avoiding buffer overflow
 char* getCMD() {
 
-  char* input;
-  int ch;
-  int size = 64; //initial size
-  int size_increment = 16; //allocate this much additional size
-  int length = 0;
+        char* input;
+        int ch;
+        int size = 64; //initial size
+        int size_increment = 16; //allocate this much additional size
+        int length = 0;
 
-  input = realloc(NULL, size * sizeof(char));
-  if(input == NULL) {
-    perror("realloc");
-    exit(-1);
-  }
-
-  while(EOF != (ch = fgetc(stdin)) && ch != '\n') {
-
-    input[length++] = ch;
-
-    if(length == size) {
-
-            input = realloc(input, (size += size_increment) * sizeof(char));
-            if(input == NULL) {
-              perror("realloc");
-              exit(-1);
-            }
-
+        input = realloc(NULL, size * sizeof(char));
+        if(input == NULL) {
+                perror("realloc");
+                exit(-1);
         }
-  }
 
-  input[length++] = '\0';
+        while(EOF != (ch = fgetc(stdin)) && ch != '\n') {
 
-  return realloc(input, length * sizeof(char));
+                input[length++] = ch;
+
+                if(length == size) {
+
+                        input = realloc(input, (size += size_increment) * sizeof(char));
+                        if(input == NULL) {
+                                perror("realloc");
+                                exit(-1);
+                        }
+
+                }
+        }
+
+        input[length++] = '\0';
+
+        return realloc(input, length * sizeof(char));
 }
 
 /*********************************/
@@ -104,93 +105,116 @@ char* getCMD() {
 //divides user input into tokens
 char** parseCMD(char* input) {
 
-  if(input == NULL) {
-    printf(PRINT_WRONG_CMD_USAGE);
-    return NULL;
-  }
+        if(input == NULL) {
+                printf(PRINT_WRONG_CMD_USAGE);
+                return NULL;
+        }
 
-  int num_of_tokens = NUM_OF_CMD_ARGS + checkFlags(input);
+        int num_of_flags = checkFlags(input);
+        if(num_of_flags == -1) {
+                printf(PRINT_WRONG_CMD_USAGE);
+                free(input);
+                return NULL;
+        }
 
-  char** cmdTokens = (char**)malloc((num_of_tokens + 1) * sizeof(char*));
-  if(cmdTokens == NULL) {
-    perror("malloc");
-    exit(-1);
-  }
+        int num_of_tokens = NUM_OF_CMD_ARGS + num_of_flags;
+        printf("num of tokens = %d\n", num_of_tokens); //DEBUG
 
-  char* delimiters = " \n";
-  char* temp = strtok(input, delimiters);
-  int i = 0;
+        char** cmdTokens = (char**)calloc((num_of_tokens + 1), sizeof(char*));
+        if(cmdTokens == NULL) {
+                perror("calloc");
+                exit(-1);
+        }
 
-  //check if first arg isnt 'client'
-  if(strcmp(temp, CMD_PREFIX)) {
-    printf(PRINT_WRONG_CMD_USAGE);
-    free(cmdTokens);
-    free(input);
-    return NULL;
-  }
+        char* delimiters = " \n";
+        char* temp = strtok(input, delimiters);
+        int i = 0;
 
-  while(temp != NULL) {
+        //check if first arg isnt 'client'
+        if(strcmp(temp, CMD_PREFIX)) {
+                printf(PRINT_WRONG_CMD_USAGE);
+                free(cmdTokens);
+                free(input);
+                return NULL;
+        }
 
-    //check if too many arguments were passed
-    if(i == num_of_tokens) {
-      printf(PRINT_WRONG_CMD_USAGE);
-      destroy_tokens(cmdTokens);
-      free(input);
-      return NULL;
-    }
+        //extract cmd args as tokens
+        while(temp != NULL && i < num_of_tokens) {
 
-    cmdTokens[i] = (char*)malloc((strlen(temp) + 1) * sizeof(char));
-    if(cmdTokens[i] == NULL) {
-      perror("malloc");
-      exit(-1);
-    }
+                cmdTokens[i] = (char*)calloc((strlen(temp) + 1), sizeof(char));
+                if(cmdTokens[i] == NULL) {
+                        perror("calloc");
+                        exit(-1);
+                }
 
-    strcpy(cmdTokens[i], temp);
-    cmdTokens[i][strlen(temp)] = '\0';
-    temp = strtok(NULL, delimiters); //move temp to the next token
+                strcpy(cmdTokens[i], temp);
+                cmdTokens[i][strlen(temp)] = '\0';
+                temp = strtok(NULL, delimiters); //move temp to the next token
 
-    i++;
-  }
+                i++;
+        }
 
-  cmdTokens[i] = NULL; //tokens ends in NULL
+        cmdTokens[i] = NULL; //tokens ends in NULL
 
-  if(i < num_of_tokens) {
-    printf(PRINT_WRONG_CMD_USAGE);
-    destroy_tokens(cmdTokens);
-    free(input);
-    return NULL;
-  }
+        printf("i = %d\n", i); //DEBUG
+        //check if too few\many arguments were passed
+        if(i != num_of_tokens || temp != NULL) {
+                printf(PRINT_WRONG_CMD_USAGE);
+                destroy_tokens(cmdTokens);
+                free(input);
+                return NULL;
+        }
 
-  free(input);
-  return cmdTokens;
+        return cmdTokens;
 }
 
 /*********************************/
 /*********************************/
 
-
+//check flags and return how many addiotional arguments are expected
 int checkFlags(char* input) {
 
-  int num_of_flags = 0;
+        int num_of_flags = 0;
 
-  char* flag = strstr(input, HEADERS_FLAG);
-  if(flag != NULL) {
-    num_of_flags = 1;
-  }
+        char* flag = strstr(input, HEADERS_FLAG);
+        if(flag != NULL) {
 
-  flag = strstr(input, DELAY_FLAG);
-  if(flag != NULL) {
+                //check for whitespaces before & after flag
+                if(flag[-1] == ' ' && flag[2] == ' ') {
+                        num_of_flags += 1;
+                }
+        }
 
-    if(num_of_flags > 0) {
-      num_of_flags = 3;
+        flag = strstr(input, DELAY_FLAG);
+        if(flag != NULL) {
 
-    } else {
+                if(flag[-1] == ' ' && flag[2] == ' ') {
 
-      num_of_flags = 2;
+                        if(!checkTimeIntervalFormat(input, flag)) {
 
-    }
-  }
-  return num_of_flags;
+                                num_of_flags += 2;
+                        } else {
+                                return -1;
+                        }
+                }
+        }
+        return num_of_flags;
+}
+
+int checkTimeIntervalFormat(char* input, char* flag_ptr) {
+
+        char interval_string[8];
+        strncpy(interval_string, flag_ptr + 3, 8);
+        printf("interval string = \"%s\"\n", interval_string); //DEBUG
+
+        int t = sscanf(interval_string, "%d%d:%d%d:%d%d"); //TODO fix this
+        // t = strcmp("kaki", "kaki");
+        printf("t = %d\n", t); //DEBUG
+
+
+
+
+        return 0;
 }
 
 /******************************************************************************/
@@ -198,19 +222,18 @@ int checkFlags(char* input) {
 /******************************************************************************/
 
 void destroy_tokens(char** cmdTokens) {
-  printf("Destroying tokens\n"); //DEBUG
-  if(cmdTokens == NULL)
-    return;
+        // printf("Destroying tokens\n"); //DEBUG
+        if(cmdTokens == NULL)
+                return;
 
-  int i;
+        int i = 0;
 
-  while(cmdTokens[i] != NULL) {
-    printf("token[%d]\n", i); //DEBUG
-    free(cmdTokens[i]);
-    i++;
-  }
-
-  free(cmdTokens);
+        while(cmdTokens[i] != NULL) {
+                // printf("token[%d]\n", i); //DEBUG
+                free(cmdTokens[i]);
+                i++;
+        }
+        free(cmdTokens);
 }
 
 /****************************/
@@ -218,16 +241,16 @@ void destroy_tokens(char** cmdTokens) {
 /****************************/
 
 void printTokens(char** cmdTokens) {
-  printf("Tokens: ");
-  if (cmdTokens == NULL)
-    return;
+        printf("Tokens: ");
+        if (cmdTokens == NULL)
+                return;
 
-  int i = 0;
+        int i = 0;
 
-  while(cmdTokens[i] != NULL) {
-    printf("%s\t", cmdTokens[i++]);
-  }
+        while(cmdTokens[i] != NULL) {
+                printf("%s\t", cmdTokens[i++]);
+        }
 
-  printf("\n");
+        printf("\n");
 
 }
