a0441329d7bc53b58066d944ad9eee04a15e04b9
ND88 Vivobook Ubuntu <ndraiman@gmail.com>
2015-11-30 11:04:35 +0200

    changed entire implementation according to lecturer new instructions

--- client.c
+++ client.c
@@ -3,32 +3,172 @@
 #include <string.h>
 #include <netinet/in.h>
 
+#define TRUE 1
+#define FALSE 0
 
 #define RFC1123FMT "%a, %d %b %Y %H:%M:%S GMT"
 #define PRINT_WRONG_CMD_USAGE "Usage: client [-h] [-d <timeinterval>] <URL>\n"
 #define PRINT_WRONG_INPUT "wrong input\n"
 #define HEADERS_FLAG "-h"
 #define DELAY_FLAG "-d"
-#define NUM_OF_CMD_ARGS 2 //not including flags & their options
-#define CMD_PREFIX "client"
+#define TIME_INTERVAL_FORMAT "%2d:%2d:%2d"
+// #define URL_FORMAT //TODO implement URL Format
+#define NUM_OF_CMD_ARGS 2 //not including flags & options
+#define DEFAULT_PORT 80
+
+static int sDelayFlag = FALSE;
+static int sDelayIndex = -1;
+static int sHeadersFlag = FALSE;
+static int sHeadersIndex = -1;
+static int sURLIndex = -1;
 
 //Method Declarations
-void initClient();
-char* getCMD();
-char** parseCMD(char*);
-int checkFlags(char*);
-int checkTimeIntervalFormat(char*, char*);
-void destroy_tokens(char**);
+int parseCMD(int, char**);
+int checkFlags(int, char**);
+int* checkTimeIntervalFormat(char*);
+int verifyURL(char*);
 
 //DEBUG Methods
-void printTokens(char**);
 
 /******************************************************************************/
 /******************************************************************************/
 
-int main() {
+int main(int argc, char* argv[]) {
 
-        initClient();
+        if(argc < 2) {
+                printf(PRINT_WRONG_CMD_USAGE);
+                exit(-1);
+        }
+
+        if(parseCMD(argc, argv)) {
+                exit(-1);
+        }
+
+        //TODO Implement executeCMD();
+
+        return 0;
+}
+
+
+/******************************************************************************/
+/************************** Input Validation Methods **************************/
+/******************************************************************************/
+
+//divides user input into tokens
+int parseCMD(int argc, char** argv) {
+
+        if(argv == NULL) {
+                printf("argv = NULL\n"); //DEBUG
+                printf(PRINT_WRONG_CMD_USAGE);
+                return -1;
+        }
+
+        int num_of_flags = checkFlags(argc, argv);
+
+        //Number of expected arguments (according to flags found)
+        int num_of_tokens = NUM_OF_CMD_ARGS + num_of_flags;
+        printf("num of tokens = %d, argc = %d\n", num_of_tokens, argc); //DEBUG
+
+        if(num_of_tokens != argc) {
+                printf(PRINT_WRONG_CMD_USAGE);
+                return -1;
+        }
+
+        int* interval = NULL;
+        if(sDelayFlag) {
+
+                interval = checkTimeIntervalFormat(argv[sDelayIndex + 1]);
+
+                if(interval == NULL) {
+                        printf("interval == NULL\n"); //DEBUG
+                        printf(PRINT_WRONG_CMD_USAGE);
+                        return -1;
+                }
+
+        }
+
+        if(verifyURL(argv[sURLIndex])) {
+                printf(PRINT_WRONG_INPUT);
+                free(interval);
+                return -1;
+        }
+
+        if(interval != NULL) {
+                free(interval);
+        }
+        return 0;
+}
+
+/*********************************/
+/*********************************/
+
+//check flags and return how many addiotional arguments are expected
+// param input - the command the user submitted
+int checkFlags(int argc, char** argv) {
+
+        int num_of_flags = 0;
+        int i;
+
+        for(i = 1; i < argc; i++) {
+
+                if(!strcmp(argv[i], HEADERS_FLAG)) {
+                        sHeadersFlag = TRUE;
+                        sHeadersIndex = i;
+                        printf("Header Flag Found, index = %d\n", sHeadersIndex); //DEBUG
+                        num_of_flags += 1;
+
+                } else if(!strcmp(argv[i], DELAY_FLAG)) {
+                        sDelayFlag = TRUE;
+                        sDelayIndex = i;
+                        printf("Delay Flag Found, index = %d\n", sDelayIndex); //DEBUG
+                        num_of_flags += 2;
+
+                }
+        }
+
+        for(i = 1; i < argc; i++) {
+                if(i != sDelayIndex && i != sDelayIndex+1 && i != sHeadersIndex)
+                        sURLIndex = i;
+        }
+        printf("URL Index = %d\n", sURLIndex); //DEBUG
+
+        return num_of_flags;
+}
+
+/*********************************/
+/*********************************/
+
+int* checkTimeIntervalFormat(char* interval_string) {
+
+        if(interval_string == NULL)
+                return NULL;
+
+        int days, hours, mins;
+        int assigned = sscanf(interval_string, TIME_INTERVAL_FORMAT, &days, &hours, &mins);
+        printf("assigned = %d\n", assigned); //DEBUG
+        printf("days = \"%d\"\thours = \"%d\"\t mins = \"%d\"\n", days, hours, mins); //DEBUG
+
+        if(assigned == 3) {
+
+                int* time_interval = (int*)calloc(3, sizeof(int));
+                if(time_interval == NULL) {
+                        return NULL;
+                }
+
+                time_interval[0] = days;
+                time_interval[1] = hours;
+                time_interval[2] = mins;
+
+                return time_interval;
+        }
+
+        return NULL;
+}
+
+/*********************************/
+/*********************************/
+
+int verifyURL(char* url) {
 
         return 0;
 }
@@ -37,224 +177,7 @@ int main() {
 /******************************************************************************/
 /******************************************************************************/
 
-void initClient() {
-
-        char* input;
-        char** cmdTokens = NULL;
-
-        do {
-
-                destroy_tokens(cmdTokens);
-                printf("cmd: ");
-                input = getCMD();
-                // printf("input = %s\n", input); //DEBUG
-
-        } while((cmdTokens = parseCMD(input)) == NULL);
-
-        // printTokens(cmdTokens); //DEBUG
-
-
-
-        free(input);
-        destroy_tokens(cmdTokens);
-}
-
-/******************************************************************************/
-/******************************************************************************/
-/******************************************************************************/
-
-
-//Method to handle user input of unknown length while avoiding buffer overflow
-char* getCMD() {
-
-        char* input;
-        int ch;
-        int size = 64; //initial size
-        int size_increment = 16; //allocate this much additional size
-        int length = 0;
-
-        input = realloc(NULL, size * sizeof(char));
-        if(input == NULL) {
-                perror("realloc");
-                exit(-1);
-        }
-
-        while(EOF != (ch = fgetc(stdin)) && ch != '\n') {
-
-                input[length++] = ch;
-
-                if(length == size) {
-
-                        input = realloc(input, (size += size_increment) * sizeof(char));
-                        if(input == NULL) {
-                                perror("realloc");
-                                exit(-1);
-                        }
-
-                }
-        }
-
-        input[length++] = '\0';
-
-        return realloc(input, length * sizeof(char));
-}
-
-/*********************************/
-/*********************************/
-
-//divides user input into tokens
-char** parseCMD(char* input) {
-
-        if(input == NULL) {
-                printf(PRINT_WRONG_CMD_USAGE);
-                return NULL;
-        }
-
-        int num_of_flags = checkFlags(input);
-        if(num_of_flags == -1) {
-                printf(PRINT_WRONG_CMD_USAGE);
-                free(input);
-                return NULL;
-        }
-
-        //Number of expected arguments (according to flags found)
-        int num_of_tokens = NUM_OF_CMD_ARGS + num_of_flags;
-        printf("num of tokens = %d\n", num_of_tokens); //DEBUG
-
-        char** cmdTokens = (char**)calloc((num_of_tokens + 1), sizeof(char*));
-        if(cmdTokens == NULL) {
-                perror("calloc");
-                exit(-1);
-        }
-
-        char* delimiters = " \n";
-        char* temp = strtok(input, delimiters);
-        int i = 0;
-
-        //check if first arg isnt 'client'
-        if(strcmp(temp, CMD_PREFIX)) {
-                printf(PRINT_WRONG_CMD_USAGE);
-                free(cmdTokens);
-                free(input);
-                return NULL;
-        }
-
-        //extract cmd args as tokens
-        while(temp != NULL && i < num_of_tokens) {
-
-                cmdTokens[i] = (char*)calloc((strlen(temp) + 1), sizeof(char));
-                if(cmdTokens[i] == NULL) {
-                        perror("calloc");
-                        exit(-1);
-                }
-
-                strcpy(cmdTokens[i], temp);
-                cmdTokens[i][strlen(temp)] = '\0';
-                temp = strtok(NULL, delimiters); //move temp to the next token
-
-                i++;
-        }
-
-        cmdTokens[i] = NULL; //tokens ends in NULL
-
-        printf("i = %d\n", i); //DEBUG
-        //check if too few\many arguments were passed
-        if(i != num_of_tokens || temp != NULL) {
-                printf(PRINT_WRONG_CMD_USAGE);
-                destroy_tokens(cmdTokens);
-                free(input);
-                return NULL;
-        }
-
-        //TODO verify URL 
-
-        return cmdTokens;
-}
-
-/*********************************/
-/*********************************/
-
-//check flags and return how many addiotional arguments are expected
-int checkFlags(char* input) {
-
-        int num_of_flags = 0;
-
-        char* flag = strstr(input, HEADERS_FLAG);
-        if(flag != NULL) {
-
-                //check for whitespaces before & after flag
-                if(flag[-1] == ' ' && flag[2] == ' ') {
-                        num_of_flags += 1;
-                }
-        }
-
-        flag = strstr(input, DELAY_FLAG);
-        if(flag != NULL) {
-
-                if(flag[-1] == ' ' && flag[2] == ' ') {
-
-                        if(!checkTimeIntervalFormat(input, flag)) {
-
-                                num_of_flags += 2;
-                        } else {
-                                return -1;
-                        }
-                }
-        }
-        return num_of_flags;
-}
-
-/*********************************/
-/*********************************/
-
-int checkTimeIntervalFormat(char* input, char* flag_ptr) {
-
-        char interval_string[8];
-        strncpy(interval_string, flag_ptr + 3, 8);
-        printf("interval string = \"%s\"\n", interval_string); //DEBUG
-
-        //TODO find way to verify format using return value without assigning it to variables
-        int days, hours, mins;
-        int assigned = sscanf(interval_string, "%2d:%2d:%2d", &days, &hours, &mins);
-        printf("assigned = %d\n", assigned); //DEBUG
-        printf("days = \"%d\"\thours = \"%d\"\t mins = \"%d\"\n", days, hours, mins); //DEBUG
-        return assigned == 3 ? 0 : -1;
-}
-
-/******************************************************************************/
-/******************************************************************************/
-/******************************************************************************/
-
-void destroy_tokens(char** cmdTokens) {
-        // printf("Destroying tokens\n"); //DEBUG
-        if(cmdTokens == NULL)
-                return;
-
-        int i = 0;
-
-        while(cmdTokens[i] != NULL) {
-                // printf("token[%d]\n", i); //DEBUG
-                free(cmdTokens[i]);
-                i++;
-        }
-        free(cmdTokens);
-}
 
 /****************************/
 /****** DEBUG Methods *******/
 /****************************/
-
-void printTokens(char** cmdTokens) {
-        printf("Tokens: ");
-        if (cmdTokens == NULL)
-                return;
-
-        int i = 0;
-
-        while(cmdTokens[i] != NULL) {
-                printf("%s\t", cmdTokens[i++]);
-        }
-
-        printf("\n");
-
-}
